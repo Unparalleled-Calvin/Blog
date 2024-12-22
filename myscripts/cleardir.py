aec1043b43d56905f1eb2abcd8429167f7413b42
Calvin <CalvinCui@foxmail.com>
2024-12-22 12:54:13 +0800

    update: script

--- myscripts/cleardir.py
+++ myscripts/cleardir.py
@@ -11,3 +11,4 @@ for file in files:
     if os.path.isdir(file_path):
         if len(os.listdir(file_path)) == 0: #移除空文件夹
             os.removedirs(file_path)
+            print(f"{file_path} is removed.")
